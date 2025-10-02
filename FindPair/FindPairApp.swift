550fdbc2942f63aff28a40df6a08d886dd15ae46
6ecool <kerimbekbekgan@gmail.com>
2025-10-03 00:09:43 +0500

    Initial Commit

---
+++ FindPair/FindPairApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindPairApp.swift
+//  FindPair
+//
+//  Created by Bekzhan Kerimbek on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FindPairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindPairTests/FindPairTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindPairTests.swift
+//  FindPairTests
+//
+//  Created by Bekzhan Kerimbek on 03.10.2025.
+//
+
+import Testing
+@testable import FindPair
+
+struct FindPairTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
